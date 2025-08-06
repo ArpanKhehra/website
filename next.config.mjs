e3ab65d3fa8fc470c93adcab94e739c49314d1e6
Pawan Kumar <surpawan@gmail.com>
2025-08-06 13:14:01 +0530

    remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // This can help when exporting as a static site
+  // output: "export", // This can help when exporting as a static site
   trailingSlash: true, // This ensures URLs have a trailing slash
 
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
   },
   images: {
-    domains: ["cdn.builder.io"],
-    unoptimized: true,
-  },
-};
+    domains: ['cdn.builder.io'],
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+export default nextConfig
